e15f7c28bc9d9988e12b3fc2dd283fc902a7f8ad
Patrick Mason <patrickmason73@gmail.com>
2023-05-01 21:52:33 -0600

    commit tree b11ed6220b61e03ee377b467e5ab3bdcd5dd8d68

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('mr bear', 3, 'M', 'brown', 'really cool guy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('mr bear', 3, 'M', 'brown', 'really cool guy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('mr bear', 3, 'M', 'brown', 'really cool guy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('mr bear', 3, 'M', 'brown', 'really cool guy', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('mr bear', 3, 'M', 'brown', 'really cool guy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('mr bear', 3, 'M', 'brown', 'really cool guy', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('mr bear', 3, 'M', 'brown', 'really cool guy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (null, 3, 'M', 'brown', 'really cool guy', 1);
